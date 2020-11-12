5df9066bd887395bbffc011333f9bff7c7906f1a
rvalessandro <ralessandro939@gmail.com>
2020-11-12 08:27:59 +0800

    Modified project structure

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -4,9 +4,4 @@ import "github.com/rvalessandro/mf-backend/controllers/customers"
 
 func mapUrls() {
 	router.GET("/customers/:customer_id", customers.Get)
-	// router.GET("/users", users.Search)
-	// router.POST("/users", users.Create)
-	// router.PUT("/users/:user_id", users.Update)
-	// router.PATCH("/users/:user_id", users.Update)
-	// router.DELETE("/users/:user_id", users.Delete)
 }
