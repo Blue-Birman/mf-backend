c0f9d1b82cde99f374b4be2c5c3cdd649b698d3d
rvalessandro <ralessandro939@gmail.com>
2020-12-08 23:16:39 +0800

    Created singleton pattern on application.go

--- app/application.go
+++ app/application.go
@@ -1,12 +1,32 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"sync"
+)
 
 var (
-	router = gin.Default()
+	router *gin.Engine
+	lock   = &sync.Mutex{}
 )
+
+func init() {
+	router, _ = generateEngine()
+}
 
 func StartApplication() {
 	mapURLs()
 	router.Run(":8080")
+}
+
+func generateEngine() (*gin.Engine, error) {
+	if router == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		router = gin.Default()
+	} else {
+		return nil, errors.New("engine already exist")
+	}
+	return router, nil
 }
